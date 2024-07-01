04ce1949990919463be3537003c54b608ab299dd
klausreus <klaus-reus@mail.de>
2024-07-01 07:42:23 +0000

    add some stuff

--- nbdev_hello/_modidx.py
+++ nbdev_hello/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://klausreus.github.io',
                 'git_url': 'https://github.com/klausreus/nbdev_hello',
                 'lib_path': 'nbdev_hello'},
-  'syms': {'nbdev_hello.core': {'nbdev_hello.core.foo': ('core.html#foo', 'nbdev_hello/core.py')}}}
+  'syms': { 'nbdev_hello.process_images': { 'nbdev_hello.process_images.add_2': ('core.html#add_2', 'nbdev_hello/process_images.py'),
+                                            'nbdev_hello.process_images.say_hello': ( 'core.html#say_hello',
+                                                                                      'nbdev_hello/process_images.py')}}}
